cce223b76b3c226353510f6ea3898547a7f4bcc7
timepasser00 <59355713+timepasser00@users.noreply.github.com>
2022-09-21 07:47:49 +0530

    Create base.sol

---
+++ contracts/base.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+import "../contracts/permission.sol";
+import "../contracts/updateReport.sol";
+
+contract base is permission{
+
+    // sets the owner of the network
+    string id_hash;
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+        isOwner[msg.sender] = true;
+    }
+
+}
